58d0cdef19ba9a8fc523fbcfc7b2ff37d9c9bb18
enrique.munguia <enrique.munguia@globant.com>
2016-04-22 00:30:55 -0500

    Initial Commit

---
+++ source/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {render} from 'react-dom';
+import AnimatedShoppingList from './AnimatedShoppingList';
+
+render(<AnimatedShoppingList />, document.getElementById('root'));
